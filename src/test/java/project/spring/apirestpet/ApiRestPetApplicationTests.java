54ab6eeac1ca5ce2e35400ab91a9fd52c3c0d019
Nicolas <aponcenicolas@gmail.com>
2020-11-20 20:17:24 -0500

    api rest test restController

--- src/test/java/project/spring/apirestpet/ApiRestPetApplicationTests.java
+++ src/test/java/project/spring/apirestpet/ApiRestPetApplicationTests.java
@@ -1,13 +1,18 @@
 package project.spring.apirestpet;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiRestPetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        int quantity = 10;
+        double price = 72.99;
+        double result = quantity * price;
+        Assertions.assertEquals(result, quantity * price);
+    }
 
 }
